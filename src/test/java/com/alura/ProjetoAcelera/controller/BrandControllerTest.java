5b8829c77194dc1bace0824cd79d108a0b94f280
Luiza Abreu <luiza@MacBook-Pro-de-Luiza.local>
2021-06-10 11:58:43 -0300

    inicio testes brand controller

---
+++ src/test/java/com/alura/ProjetoAcelera/controller/BrandControllerTest.java
@@ -0,0 +1,97 @@
+package com.alura.ProjetoAcelera.controller;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+// @ActiveProfiles("test")
+public class BrandControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void registerBrandTest()
+            throws Exception {
+        URI uri = new URI("/brands");
+        String json = "{\"name\":\"teste\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(uri)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(201));
+    }
+
+    @Test
+    public void updateBrandTest()
+            throws Exception {
+        URI uri = new URI("/brands/3");
+        String json = "{\"name\":\"teste\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .put(uri)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200));
+    }
+
+    @Test
+    public void getAllBrandsTest()
+            throws Exception {
+        URI uri = new URI("/brands");
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get(uri))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200));
+    }
+
+    @Test
+    public void getBrandTest()
+            throws Exception {
+        URI uri = new URI("/brands/1");
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get(uri))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200));
+    }
+
+    @Test
+    public void deleteBrandTest()
+            throws Exception {
+        URI uri = new URI("/brands/6");
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete(uri))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200));
+    }
+}
---
+++ src/test/java/com/alura/ProjetoAcelera/repository/BrandRepositoryTests.java
@@ -0,0 +1,23 @@
+package com.alura.ProjetoAcelera.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class BrandRepositoryTests {
+    @Autowired private BrandRepository brandRepository;
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
