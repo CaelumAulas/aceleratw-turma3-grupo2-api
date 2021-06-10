c7289aae2f1511e4e0c0e435212574fbbdd48c20
Luiza Abreu <luiza@MacBook-Pro-de-Luiza.local>
2021-06-10 15:36:30 -0300

    testes de repository acessando banco de teste

--- src/test/java/com/alura/ProjetoAcelera/repository/BrandRepositoryTests.java
+++ src/test/java/com/alura/ProjetoAcelera/repository/BrandRepositoryTests.java
@@ -7,18 +7,26 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 public class BrandRepositoryTests {
     @Autowired private BrandRepository brandRepository;
+    @Autowired private TestEntityManager em;
 
     @Test
     public void findByNameTest(){
         String name = "marca 1";
+
+        Brand brandTest = new Brand();
+        brandTest.setName(name);
+        em.persist(brandTest);
+
         Brand brand = brandRepository.findByName(name);
         Assert.assertNotNull(brand);
         Assert.assertEquals(name, brand.getName());
--- src/test/java/com/alura/ProjetoAcelera/repository/UserRepositoryTests.java
+++ src/test/java/com/alura/ProjetoAcelera/repository/UserRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.alura.ProjetoAcelera.repository;
 
+import com.alura.ProjetoAcelera.models.Brand;
 import com.alura.ProjetoAcelera.models.User;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,17 +8,26 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 public class UserRepositoryTests {
     @Autowired private UserRepository userRepository;
+    @Autowired private TestEntityManager em;
+
     @Test
     public void findByNameTest(){
         String name = "luiza";
+
+        User userTest = new User();
+        userTest.setName(name);
+        em.persist(userTest);
+
         User user = userRepository.findByName(name);
         Assert.assertNotNull(user);
         Assert.assertEquals(name, user.getName());
